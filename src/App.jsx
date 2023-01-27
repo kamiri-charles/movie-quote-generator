c829a0e7b1a91d19519e038086965fff9a89f351
Charles Kamiri <charleskamiri6@gmail.com>
2023-01-28 00:49:31 +0300

    Fixed contributors view design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,42 @@
 import { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faRefresh } from "@fortawesome/free-solid-svg-icons"
+import { faCircleInfo, faRefresh } from "@fortawesome/free-solid-svg-icons"
 import { faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { faCopy } from "@fortawesome/free-regular-svg-icons"
 import Loader from "react-loaders"
 import quotes from "./quotes.json"
 import "./App.scss";
+import Contributor from "./Contributor"
+import { useRef } from "react"
 
 const App = () => {
 	const [data, setData] = useState(null);
+	let [contributors, setContributors] = useState();
+	let contrib = useRef();
+	
 	
 	const load_quote = () => {
 		setData(null);
 		setTimeout(() => {
 			setData(quotes[Math.floor(Math.random() * quotes.length)])
 		}, 3000)
-
+		
 	};
+	
+	
+	const fetch_contributors = async () => {
+		fetch('https://api.github.com/repos/kamiri-charles/movie-quote-generator/contributors')
+		.then(res => res.json())
+		.then(data => setContributors(data))
+	}
 
-	const requestContributors = () => {
-		return Promise.resolve(fetch(`https://api.github.com/repos/kamiri-charles/movie-quote-generator/contributors`)
-		.then(response => response.json())
-		.then(function (data){
-			let noDoubles = document.getElementsByClassName('noDoubles');
-			if (noDoubles.length === 0) {
-				for (let i = 0; i < data.length; i++) {
-				let contUrl = data[i].html_url;
-				let contAva = data[i].avatar_url;
-				let clist = document.querySelector('.contList');
-				let aEl = document.createElement('a');
-				let imgEl = document.createElement('img');
-				clist.appendChild(aEl);
-				aEl.append(imgEl)
-				aEl.href = contUrl;
-				imgEl.href= contUrl;
-				imgEl.src = contAva;
-				imgEl.className = 'contributor-avatar noDoubles'
-				}
-			} else {}
-	}));
-}
-
-
+	let contrib_view = () => {
+		if (contrib.current.classList.contains('hidden')) {
+			contrib.current.classList.remove('hidden')
+		} else {contrib.current.classList.add('hidden')}
+	}
+	
+	
 	/* Copy quote */
 	const copy_quote = () => {
 		navigator.clipboard.writeText(data.quote + " - " + data.origin);
@@ -51,12 +46,13 @@ const App = () => {
 	
 	useEffect(() => {
 		load_quote();
-		requestContributors();
+		fetch_contributors();
 	}, []);
-
-
+	
+	
 	return (
 		<div className="App">
+
 			<div className='title'>Random Movie Quote Generator</div>
 
 			<div className='wrapper'>
@@ -67,29 +63,38 @@ const App = () => {
 							<div className='quote'>{data.quote}</div>
 							<div className='origin'>{data.movie + " ~ " + data.year}</div>
 						</>
-					) }
+						) }
 				</div>
-
-				<div className='buttons'>
-					<button onClick={load_quote} disabled={!data}>
-						<FontAwesomeIcon icon={faRefresh} color='white' />
-					</button>
-
-					<a href='https://twitter.com/intent/tweet' target='_blank' rel='noreferrer'>
+					
+					<div className='buttons'>
+						<button onClick={load_quote} disabled={!data}>
+							<FontAwesomeIcon icon={faRefresh} color='white' />
+						</button>
+					
+						<a href='https://twitter.com/intent/tweet' target='_blank' rel='noreferrer'>
 							<FontAwesomeIcon icon={faTwitter} color='white' />
-					</a>
-
-					<button onClick={copy_quote}>
-						<FontAwesomeIcon icon={faCopy} color='white' />
-					</button>
+						</a>
+					
+						<button onClick={copy_quote}>
+							<FontAwesomeIcon icon={faCopy} color='white' />
+						</button>
+					</div>
+				
 				</div>
 
+				<div className="info" onClick={contrib_view}>
+					<FontAwesomeIcon icon={faCircleInfo} color='blue' />
+				</div>
+
+				<div className="contributors hidden" ref={contrib}>
+					<span>Contributors</span>
+					{!contributors ? <Loader type="ball-pulse" /> : contributors.map((c, idx) => (
+						<Contributor data={c} key={idx}/>
+					))}
+				</div>
 			</div>
-			<div className="contributors">Contributors:</div>
-			<ul className="contList">
-			</ul>
-		</div>
-		);
-	};
-	
-	export default App;
+			
+			);
+		};
+		
+		export default App;
