b91f2b73ea62b6f213e22ddf35268b4c55369b7e
Charles Kamiri <charleskamiri6@gmail.com>
2023-02-08 23:26:43 +0300

    Minor bug fixes.

--- src/components/Contributors/Contributor.jsx
+++ src/components/Contributors/Contributor.jsx
@@ -1,6 +1,6 @@
 const Contributor = ({ data }) => {
   return (
-    <a className="contributor" href={data.html_url} target="_blank">
+    <a className="contributor" href={data.html_url} target="_blank" rel="noreferrer">
       <img src={data.avatar_url} alt="contributor-avatar" />
       <span>{data.login}</span>
     </a>
